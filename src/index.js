db80e0a60944390a37c189229346dc9c2e8de414
MegamiAy <megami.ay@gmail.com>
2023-04-12 15:45:33 -0300

    s

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CreateUser from "./screens/CreateUser";
+import { NavigationContainer } from "@react-navigation/native";
 
-
-
-
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function RootNavigation() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="CreateUser" component={CreateUser} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Cadastro" component={CreateUser} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- src/screens/CreateUser.jsx
+++ src/screens/CreateUser.jsx
@@ -1,19 +1,40 @@
 import { View } from "react-native";
 import { Button, Text, TextInput } from "react-native-paper";
-
-
-
+import { styles } from "../config/styles";
+import { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../config/firebase";
 
 export default function CreateUser() {
+    const [nome, setNome] = useState("");
+
+    function cadastrarPessoa() {
+        console.log(nome);
+
+        const docRef = addDoc(
+            collection(db, "pessoas"),
+            {
+                nomeDaPessoa: nome
+            }
+        )
+    };
+
     return(
-        <View>
+        <View 
+            style={styles.container}
+        >
             <Text>Criar Usuario</Text>
             <View>
                 <TextInput
                     label={"Nome"}
+                    placeholder="Digite seu nome"
+                    value={nome}
                     mode="outlined"
+                    onChangeText={setNome}
                 />
-                <Button>Cadastrar</Button>
+                <Button
+                    onPress={cadastrarPessoa}
+                >Cadastrar</Button>
             </View>
         </View>
     )
